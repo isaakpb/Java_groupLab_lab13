a7bf8477b99469bcb6e86dd6c82c95436d03efbf
justmeeos <justmeeos@home.com>
2022-11-12 22:16:47 -0500

    Finished Room.java. No testing done

---
+++ Rooms_andMore/Classroom.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Classroom extends Room{
+    int numberOfChairs;
+
+    public Classroom(int area){
+        super(area);
+    }
+
+}
---
+++ Rooms_andMore/Room.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Room {
+   int area;
+
+   public Room(int area) {
+      this.area = area;
+   }
+
+   public int getSquareFeet() {
+       return this.area;
+   }
+   public void setSquareFeet(int area) {
+       this.area = area;
+   }
+   public int getCapacity(){
+       return this.area / 9;
+   }
+
+   public static String toString(){
+       return "Room: " + this.area + " square feet" + " Capcity: " + this.getCapacity();
+   }
+}
