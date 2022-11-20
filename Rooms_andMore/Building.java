1e241ae428d5ab024b77af9a702ac36c0260fa46
justmeeos <justmeeos@home.com>
2022-11-20 14:27:40 -0500

    added interactivity with the building.java file.

--- Rooms_andMore/Building.java
+++ Rooms_andMore/Building.java
@@ -5,7 +5,11 @@ public class Building {
 
 
     // This contians all the elvators created
-    ArrayList<Elevator> elevators = new ArrayList<Elevator>();
+    ArrayList<Elevator> elevators;
+    public Building(){
+        this.classrooms = new ArrayList<Classroom>();
+        this.elevators = new ArrayList<Elevator>();
+    }
     public static void main(String[] args) {
         Scanner kybd = new Scanner(System.in);
         Building myBuilding = new Building();
@@ -13,19 +17,19 @@ public class Building {
         Random rand = new Random();
 
         System.out.println("Enter \n\t1: create classroom\n\t2: create create elevator" +"\n\t3: Look up Elevator, Classroom and Room information" + "\n\t4: exit");
-        int inp = kybd.nextInt();
+        int inp =Integer.parseInt(kybd.nextLine());
         while (inp != 4) {
             if (inp == 1) {
                 int area = 100;
                 System.out.println("If the classroom is the standrad size (100 sq ft) enter y . If it is a custom size enter n:  y/n");
                 if (kybd.next().equals("n")) {
                     System.out.println("Enter the area of the classroom: ");
-                    area = kybd.nextInt();
+                    area =Integer.parseInt(kybd.nextLine());
                 }
                 System.out.println("Are there any chairs in the classroom? y/n");
                 if (kybd.next().equals("y")) {
                     System.out.println("Enter the number of chairs in the classroom: ");
-                    int chairs = kybd.nextInt();
+                    int chairs =Integer.parseInt(kybd.nextLine());
                     Classroom classTestNumberChairs = new Classroom(area, chairs);
                     System.out.println(classTestNumberChairs);
                     myBuilding.classrooms.add(classTestNumberChairs);
@@ -37,16 +41,97 @@ public class Building {
 
 
             } else if (inp == 2) {
-                Elevator current = new Elevator(rand.nextInt(100) + 10);
-                if (rand.nextInt(2) == 0) {
-                    current.up(rand.nextInt(10));
-                } else {
-                    current.down(rand.nextInt(10));
+                int elevatorArea = 20;
+                System.out.println("Is the elevator the standard size (20 sq ft) enter y . If it is a custom size enter n:  y/n");
+                if (kybd.next().equals("n")) {
+                    System.out.println("Enter the area of the elevator: ");
+                    elevatorArea =Integer.parseInt(kybd.nextLine());
                 }
+                Elevator current = new Elevator(elevatorArea);
+                myBuilding.elevators.add(current);
+                System.out.println("Would you like to move the elevator up or down (starts on floor one) up u/down d");
+                String next = kybd.nextLine();
+                if (next.equals("u")) {
+                    System.out.println("Enter the number of floors you would like to move up: ");
+                    int floors =Integer.parseInt(kybd.nextLine());
+                    current.up(floors);
+                    System.out.println(current);
+                    myBuilding.elevators.add(current);
+                } else if (next.equals("d")) {
+                    System.out.println("Enter the number of floors you would like to move down: ");
+                    int floors =Integer.parseInt(kybd.nextLine());
+                    current.down(floors);
+                    System.out.println(current);
+                    myBuilding.elevators.add(current);
+                }
+
                 // add current to the ArrayList
-            }
-            System.out.println("Enter \n\t1: create classroom\n\t2: create create elevator" +"\n\t3: Look up Elevator, Classroom and Room information" + "\n\t4: exit");
-            inp = kybd.nextInt();
+            }else if (inp == 3){
+                int numberOfClassRooms = myBuilding.classrooms.size();
+                int numberOfElevators = myBuilding.elevators.size();
+                System.out.println("There are " + numberOfClassRooms + " classrooms and " + numberOfElevators + " elevators in the building");
+                System.out.println("Would you like to look at a classroom, elevator or room? c/e/r");
+                String next = kybd.nextLine();
+                if (next.equals("c")){
+
+
+                    System.out.println("There are " + numberOfClassRooms + " classrooms in the building enter a number between 1 and " + numberOfClassRooms + " to look at a specific classroom");
+
+                    int classNum =Integer.parseInt(kybd.nextLine());
+                    Classroom currentRoom = myBuilding.classrooms.get(classNum - 1);
+                    int inputCode = 0;
+                    do{
+                        System.out.println("Would you like to\n 1. Get the number of chairs. \n 2. Get room capacity. \n3. Set the number of chairs. \n 0. Exit");
+                        inputCode =Integer.parseInt(kybd.nextLine());
+                        if (inputCode == 1){
+                            System.out.println("There are " + currentRoom.getNumberOfChairs() + " chairs in the classroom");
+                        }else if (inputCode == 2){
+                            System.out.println("The capacity of the classroom is " + currentRoom.getCapacity());
+                        }else if (inputCode == 3){
+                            System.out.println("Enter the number of chairs in the classroom: ");
+                            int chairs =Integer.parseInt(kybd.nextLine());
+                            currentRoom.setNumOfChairs(chairs);
+                        }
+
+                    }while(inputCode != 0);
+
+                    System.out.println(myBuilding.classrooms.get(classNum));
+            } else if (next.equals("e")) {
+                    System.out.println("There are " + numberOfElevators + " elevators in the building enter a number between 1 and " + numberOfElevators + " to look at a specific elevator");
+                    int elevatorNum = 0;
+                    do {
+                        System.out.println("Enter an existing elevator number to contine: ");
+                        elevatorNum = Integer.parseInt(kybd.nextLine());
+                    } while (elevatorNum < 1 || elevatorNum > numberOfElevators);
+
+                    Elevator currentElevator = myBuilding.elevators.get(elevatorNum - 1);
+                    System.out.println(currentElevator.toString());
+                    System.out.println("Would you like to move the elevator up or down y/n");
+                    String next2 = kybd.nextLine();
+                    if (next2.equals("y")) {
+
+
+                        System.out.println("Would you like to move the elevator up or down (starts on floor one) up u/down d");
+                        String next3 = kybd.nextLine();
+                        if (next3.equals("u")) {
+                            System.out.println("Enter the number of floors you would like to move up: ");
+                            int floors = Integer.parseInt(kybd.nextLine());
+                            currentElevator.up(floors);
+                            System.out.println(currentElevator);
+
+                        } else if (next3.equals("d")) {
+                            System.out.println("Enter the number of floors you would like to move down: ");
+                            int floors = Integer.parseInt(kybd.nextLine());
+                            currentElevator.down(floors);
+                            System.out.println(currentElevator);
+
+                        }
+                    }
+                }
+
+                }
+                System.out.println("Enter \n\t1: create classroom\n\t2: create create elevator" +"\n\t3: Look up Elevator, Classroom and Room information" + "\n\t4: exit");
+            inp =Integer.parseInt(kybd.nextLine());
         }
         kybd.close();
         // create a for loop to walk through the ArrayList its elements, one per line
